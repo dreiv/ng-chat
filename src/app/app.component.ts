a3d2bed942a852ec965feb727a43d94f19e246cf
Andrei Voicu <dreivoicu@gmail.com>
2020-06-13 11:20:37 +0300

    connected to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBlDHJA4YHsppM7m2Q4nOWNjVr7PvusYzA',
+  databaseURL: 'https://ngchat-6dcba.firebaseio.com'
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
