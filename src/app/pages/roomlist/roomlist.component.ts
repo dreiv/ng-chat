8c802429423cd03d6f8bf2e143a1f0b1c4bc732a
Andrei Voicu <dreivoicu@gmail.com>
2020-06-13 15:45:05 +0300

    added roomlist

--- src/app/pages/roomlist/roomlist.component.ts
+++ src/app/pages/roomlist/roomlist.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import * as firebase from 'firebase';
+import { DatePipe } from '@angular/common';
+
+export const snapshotToArray = (snapshot: any) => {
+  const returnArr = [];
+
+  snapshot.forEach((childSnapshot: any) => {
+    const item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr;
+};
 
 @Component({
   selector: 'app-roomlist',
@@ -6,7 +21,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roomlist.component.scss']
 })
 export class RoomlistComponent implements OnInit {
-  constructor() {}
+  nickname = '';
+  displayedColumns: string[] = ['roomname'];
+  rooms = [];
+  isLoadingResults = true;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public datepipe: DatePipe
+  ) {
+    this.nickname = localStorage.getItem('nickname');
+
+    firebase
+      .database()
+      .ref('rooms/')
+      .on('value', resp => {
+        this.rooms = [];
+        this.rooms = snapshotToArray(resp);
+        this.isLoadingResults = false;
+      });
+  }
 
   ngOnInit(): void {}
+
+  enterChatRoom(roomname: string): void {
+    const chat = {
+      roomname: roomname,
+      nickname: this.nickname,
+      message: this.datepipe.transform(new Date(), 'dd/MM/yyyy HH:mm:ss'),
+      date: `${this.nickname} enter the room`,
+      type: 'join'
+    };
+    const newMessage = firebase
+      .database()
+      .ref('chats/')
+      .push();
+    newMessage.set(chat);
+
+    firebase
+      .database()
+      .ref('roomusers/')
+      .orderByChild('roomname')
+      .equalTo(roomname)
+      .on('value', (resp: any) => {
+        let roomuser = [];
+        roomuser = snapshotToArray(resp);
+        const user = roomuser.find(x => x.nickname === this.nickname);
+
+        if (user) {
+          const userRef = firebase.database().ref('roomusers/' + user.key);
+          userRef.update({ status: 'online' });
+        } else {
+          const newroomuser = {
+            roomname,
+            nickname: this.nickname,
+            status: 'online'
+          };
+          const newRoomUser = firebase
+            .database()
+            .ref('roomusers/')
+            .push();
+
+          newRoomUser.set(newroomuser);
+        }
+      });
+
+    this.router.navigate(['/chatroom', roomname]);
+  }
+
+  logout(): void {
+    localStorage.removeItem('nickname');
+    this.router.navigate(['/login']);
+  }
 }
